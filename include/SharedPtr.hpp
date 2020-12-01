0e09e1307bb484f5d5b13fe98c88cf2e93e805be
pan_fera <pan_fera@mail.ru>
2020-12-01 12:11:25 +0300

    v1.3

---
+++ include/SharedPtr.hpp
@@ -0,0 +1,89 @@
+//
+// Created by hacker on 28.11.2020.
+//
+
+#ifndef SHARED_PTR_SHAREDPTR_HPP
+#define SHARED_PTR_SHAREDPTR_HPP
+
+#include <atomic>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>    // std::swap
+
+template <typename T>
+class SharedPtr {
+ public:
+  SharedPtr():m_ptr(nullptr), m_count(nullptr){std::cout << "Конструктор" <<std::endl;};
+  SharedPtr(T* ptr):m_ptr(ptr), m_count(new std::atomic_uint(1)){std::cout << "Конструктор ptr" <<std::endl;};
+  //консруктор копирования
+  SharedPtr(const SharedPtr& r):m_ptr(r.m_ptr), m_count(++*r.m_count){
+    std::cout << "Конструктор копирования" <<std::endl;
+                                                  };
+  //конструктор перемещения
+  SharedPtr(SharedPtr&& r):m_ptr(r.m_ptr), m_count(r.m_count){
+    r.m_ptr = nullptr;
+    r.m_count = nullptr;
+    std::cout << "Конструктор перемещения" <<std::endl;
+  };
+  ~SharedPtr() {
+    std::cout<<"Деструктор"<<std::endl;
+    reset();
+  }
+  //оператор копирования
+  auto operator=(const SharedPtr& r) -> SharedPtr&{
+
+    if (this != &r) {
+
+      reset();
+      m_ptr = r.m_ptr;
+      ++*r.m_count;
+      m_count = r.m_count;
+      std::cout << "Перегруженный оператор присваивания" <<std::endl;
+    }
+    else {
+      std::cout << "Самоприсваивание" << std::endl;
+    }
+    return *this;
+  }
+  auto operator=(SharedPtr&& r) -> SharedPtr&{
+    reset();
+    m_ptr = r.m_ptr;
+    m_count = r.m_count;
+    r.m_ptr = nullptr;
+    r.m_count = nullptr;
+  }
+
+  // проверяет, указывает ли указатель на объект
+  operator bool() const;
+  auto operator*() const -> T&;
+  auto operator->() const -> T*;
+
+  auto get() -> T*;
+  void reset(){
+    if(m_count != nullptr){
+      --*m_count;
+      if (*m_count == 0){
+        m_ptr->~T();
+        delete m_ptr;
+        delete m_count;
+      }
+    }
+    m_ptr = nullptr;
+    m_count = nullptr;
+  }
+  void reset(T* ptr){
+    reset();
+    m_ptr = ptr;
+    m_count = new std::atomic_uint(1);
+  }
+  void swap(SharedPtr& r){
+    std::swap(m_count, r.m_count);
+    std::swap(m_ptr, r.m_ptr);
+  }
+  // возвращает количество объектов SharedPtr, которые ссылаются на тот же управляемый объект
+//auto use_count() const -> size_t;
+ private:
+  T* m_ptr;
+  std::atomic_uint* m_count;
+};
+#endif  // SHARED_PTR_SHAREDPTR_HPP
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,6 +1,15 @@
 // Copyright 2020 Your Name <your_email>
 
-#include "header.hpp"
+#include "SharedPtr.hpp"
 int main(){
+std::cout<<"ds"<< std::endl;
+SharedPtr<int> A;
 
+  int a = 5;
+  int *s = (int*) malloc(sizeof(int));
+  *s=a;
+  SharedPtr<int> B(s);
+  A=B;
+  A=A;
+return 0;
 }
